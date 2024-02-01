d7ce2dfb4acf49affa1dde043048d453256e3449
Andreas <and@reas.me>
2024-02-01 02:03:46 -0800

    Add responsive support; fix style

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 A simple web representation of the Squid Game Grid.
 
+Designed to be responsive.
+
 Just a POC, the code is inelegant.
+
+## Config
+
+See main.js to set the `NUM_OF_PLAYERS`.
+
+Only 1 to 100 are tested.
 
 Ref: <https://www.youtube.com/watch?v=0-Azzeh4b2g>
--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-const N = 7 * 7;
-let nums = Array.from({ length: N }, (_, i) => i + 1);
-for (let i = 0; i < N; i++) {
-    let j = Math.floor(Math.random() * N);
+const NUM_OF_PLAYERS = 37;
+const SQUARE_SIDE_SIZE = Math.ceil(Math.sqrt(NUM_OF_PLAYERS));
+const NUM_OF_SQUARES = SQUARE_SIDE_SIZE ** 2;
+
+let nums = Array.from({ length: NUM_OF_SQUARES }, (_, i) => i + 1);
+
+// shuffle nums
+for (let i = 0; i < NUM_OF_SQUARES; i++) {
+    let j = Math.floor(Math.random() * NUM_OF_SQUARES);
     [nums[i], nums[j]] = [nums[j], nums[i]];
 }
 
-
-document.addEventListener("DOMContentLoaded", function () {
+function genSquares() {
     let screen = document.getElementById("screen");
 
-    for (let i = 0; i < 7 * 7; i++) {
+    for (let i = 0; i < NUM_OF_SQUARES; i++) {
         let squareDiv = document.createElement("div");
         squareDiv.classList.add("square");
+
+        if (nums[i] > NUM_OF_PLAYERS) {
+            squareDiv.classList.add("gone");
+            screen.appendChild(squareDiv);
+            continue;
+        }
 
         let picDiv = document.createElement("div");
         picDiv.classList.add("pic");
 
         let textDiv = document.createElement("div");
         textDiv.classList.add("text");
-        textDiv.textContent = nums[i];
+        textDiv.textContent = nums[i].toString().padStart(3, "0");
 
         squareDiv.appendChild(picDiv);
         squareDiv.appendChild(textDiv);
@@ -29,4 +39,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
         screen.appendChild(squareDiv);
     }
+
+}
+
+let style = document.createElement('style');
+function setDimensions() {
+    let screen = document.getElementById("screen");
+    screen.style.gridTemplateColumns = `repeat(${SQUARE_SIDE_SIZE}, 1fr)`;
+
+    //remove all rules from style
+    while (style.sheet.cssRules.length > 0) {
+        style.sheet.deleteRule(0);
+    }
+
+    let vx = Math.floor(100 / (SQUARE_SIDE_SIZE + 3)) - 1;
+    let use_vw_or_vh = window.screen.height < window.screen.width ? "vh" : "vw";
+
+    // insert rule into style
+    style.sheet.insertRule(`.square {height: ${vx}${use_vw_or_vh};}`);
+    style.sheet.insertRule(`.text {transform: rotate(45deg) translateY(${vx / 3}${use_vw_or_vh});}`);
+    style.sheet.insertRule(`.text {font-size: ${vx / 3}${use_vw_or_vh};}`);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    genSquares();
+
+    document.head.appendChild(style);
+    setDimensions();
+
+    onresize = (event) => {
+        setDimensions();
+    };
 });
