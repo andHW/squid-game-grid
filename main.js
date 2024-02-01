b0767d82d0a29b7138361bab4ef06ee249508a77
Andreas <and@reas.me>
2024-02-01 00:46:46 -0800

    Shrink board; Randomize nums; Refine css

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+const N = 7 * 7;
+let nums = Array.from({ length: N }, (_, i) => i + 1);
+for (let i = 0; i < N; i++) {
+    let j = Math.floor(Math.random() * N);
+    [nums[i], nums[j]] = [nums[j], nums[i]];
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
     let screen = document.getElementById("screen");
 
-    for (let i = 0; i < 64; i++) {
+    for (let i = 0; i < 7 * 7; i++) {
         let squareDiv = document.createElement("div");
         squareDiv.classList.add("square");
 
@@ -10,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         let textDiv = document.createElement("div");
         textDiv.classList.add("text");
-        textDiv.textContent = i;
+        textDiv.textContent = nums[i];
 
         squareDiv.appendChild(picDiv);
         squareDiv.appendChild(textDiv);
